24fdf18a19eb8826b3dcf98834bfc9663729457a
mosio14 <mostafa.asadollahy@gmail.com>
2024-08-31 17:21:46 +0200

    add navbarmenu

---
+++ src/components/NavbarMenu.tsx
@@ -0,0 +1,213 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import {Button} from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import LanguageIcon from '@mui/icons-material/Language';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import MoreIcon from '@mui/icons-material/MoreVert';
+
+
+const navItems = ['Body shape', 'Clothing', 'Accessories', 'Sale'];
+
+export default function NavbarMenu() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+        React.useState<null | HTMLElement>(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton size="large" color="inherit">
+                    <Badge  color="error">
+                        <SearchIcon />
+                    </Badge>
+                </IconButton>
+                <p>Search</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton size="large" color="inherit">
+                    <Badge  color="error">
+                        <LanguageIcon />
+                    </Badge>
+                </IconButton>
+                <p>Language</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton size="large" aria-label="Items you like" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <FavoriteBorderIcon />
+                    </Badge>
+                </IconButton>
+                <p>Favourite</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="Cart contains 3 items"
+                    color="inherit"
+                >
+                    <Badge badgeContent={3} color="error">
+                        <ShoppingBagIcon />
+                    </Badge>
+                </IconButton>
+                <p>Cart</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{backgroundColor: 'white', color:'black', height: '20vh'}}>
+                <Toolbar>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {navItems.map((item) => (
+                            <Button key={item} sx={{ color: '#000' }}>
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Typography
+                            variant="h4"
+                            noWrap
+                            component="div"
+                            sx={{ display: { xs: 'none', sm: 'block' }, textAlign: 'center',fontWeight: 'bold' }}
+                        >
+                            Shop By Shape
+                        </Typography>
+                    </Box>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <IconButton size="large"  color="inherit">
+                            <Badge >
+                                <SearchIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton size="large"  color="inherit">
+                            <Badge  color="error">
+                                <LanguageIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                            <Badge badgeContent={4} color="error">
+                                <FavoriteBorderIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            aria-label="cart contains 3 items"
+                            color="inherit"
+                        >
+                            <Badge badgeContent={3} color="error">
+                                <ShoppingBagIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </Box>
+    );
+}
